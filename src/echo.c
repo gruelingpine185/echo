5396b23977f8f77d1021e75cbce08741debdadf5
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2023-07-16 11:47:25 -0400

    changed internal buffer to use ctx->buffer

--- src/echo.c
+++ src/echo.c
@@ -1,13 +1,22 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "echo.h"
 
 
 int echo_create_ctx(echo_ctx* _ctx) {
     if(!_ctx || !_ctx->info) return 0;
+    if(_ctx->info->len < BUFSIZ) return 0;
 
     _ctx->buffer = (char*) malloc(sizeof(char) * _ctx->info->len);
     if(!_ctx->buffer) return 0;
+
+    memset(_ctx->buffer, 0, _ctx->info->len);
+    if(setvbuf(_ctx->info->fs, _ctx->buffer, _IOFBF, _ctx->info->len) != 0) {
+        echo_destroy_ctx(_ctx);
+        return 0;
+    }
 
     _ctx->offset = 0;
     _ctx->warnings = 0;
